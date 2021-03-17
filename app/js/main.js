2b63cea6bb8acabed4b264765d175a24306ad4bc
Semen <senia-2010@mail.ru>
2021-03-17 18:34:41 +0300

    add if for

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,6 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
-
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -13,13 +12,27 @@ const personalMovieDB = {
 };
 
 
+// for (let i = 0; i <2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+// b = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?', ''),
-d = prompt('На сколько оцените его?', '');
+// if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+// } else {
+//     console.log('error');
+//     i--;
+// }
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фидьмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы - классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы - киноман');
+} else {
+    console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
